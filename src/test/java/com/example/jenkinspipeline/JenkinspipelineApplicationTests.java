e6503778b0853c17659859637ce37d5fbce1bd7d
jwozniak <jw303282@student.polsl.pl>
2024-05-13 12:58:51 +0200

    Commit 2

--- src/test/java/com/example/jenkinspipeline/JenkinspipelineApplicationTests.java
+++ src/test/java/com/example/jenkinspipeline/JenkinspipelineApplicationTests.java
@@ -2,7 +2,6 @@ package com.example.jenkinspipeline;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.core.AutoConfigureCache;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
